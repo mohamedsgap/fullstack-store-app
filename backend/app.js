4f08ba8bd6021a4767e7095e9a67a3ac97937278
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-07-25 19:17:03 +0200

    add some middleware

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => {
+app.use((req, res,next)=>{
+    console.log("The request received");
+    next();
+});
+
+app.use((req, res, next) =>{
+    res.status(201);
+    next();
+});
+
+app.use((req, res, next) => {
     res.json({message: 'Your request was successfull! '});
+    next();
+});
+
+app.use((req, res, next)=>{
+    console.log("the rquest sent successfully");
 });
 
 module.exports = app;
