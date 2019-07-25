c34742aa209d63226e303b2c8a4e3b25bf03017a
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-07-25 18:54:14 +0200

    serving up Express app

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
 const http = require('http');
+const app = require('./app'); 
 
+app.set('port', process.env.PORT || 3000 );
+
+const server = http.createServer(app);
+
+// comment this hard coded line 
+/*
 const server = http.createServer((req, res) => {
     res.end("this is my first server response!, Mohamed hits full-stack :) ");
 });
+*/
 
 server.listen(process.env.PORT || 3000);
