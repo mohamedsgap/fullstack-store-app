279914b474cf782df3946ebfd995536b9406ecee
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-07-26 05:12:53 +0200

    create a GET route

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-
+/*
 app.use((req, res,next)=>{
     console.log("The request received");
     next();
@@ -20,5 +20,38 @@ app.use((req, res, next) => {
 app.use((req, res, next)=>{
     console.log("the rquest sent successfully");
 });
+*/
+
+app.use((req, res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+
+app.use('/api/stuff',(req, res, next) => {
+    const stuff = [
+        {
+            _id: 'oeihfzeoi',
+            title: 'My first thing',
+            description: 'All of the info about my first thing',
+            imageUrl: '',
+            price: 4900,
+            userId: 'qsomihvqios',
+          },
+          {
+            _id: 'oeihfzeomoihi',
+            title: 'My second thing',
+            description: 'All of the info about my second thing',
+            imageUrl: '',
+            price: 2900,
+            userId: 'qsomihvqios',
+          },
+    ];
+
+    res.status(200).json(stuff);
+});
+
 
 module.exports = app;
