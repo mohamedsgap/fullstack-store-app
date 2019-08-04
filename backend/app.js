4d11ec2a379b549b773a759d4759364fb4a23c94
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-08-04 07:36:11 +0200

    Retrieve a list of Things for sale

--- backend/app.js
+++ backend/app.js
@@ -67,27 +67,14 @@ app.post('/api/stuff', (req, res, next) => {
 });
 
 app.use('/api/stuff',(req, res, next) => {
-    const stuff = [
-        {
-            _id: 'oeihfzeoi',
-            title: 'My first thing',
-            description: 'All of the info about my first thing',
-            imageUrl: '',
-            price: 4900,
-            userId: 'qsomihvqios',
-          },
-          {
-            _id: 'oeihfzeomoihi',
-            title: 'My second thing',
-            description: 'All of the info about my second thing',
-            imageUrl: '',
-            price: 2900,
-            userId: 'qsomihvqios',
-          },
-    ];
-
-    res.status(200).json(stuff);
+    Thing.find().then((things)=>{
+        res.status(200).json(things);
+    }).catch((error)=>{
+        res.status(400).json({
+            error: error
+        });
+    });
 });
 
 
-module.exports = app;
+module.exports = app;   
