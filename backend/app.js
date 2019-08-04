e76fe398278afcc5cb7f9981a77cb0179ea270da
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-08-04 08:09:03 +0200

    deleting a Thing

--- backend/app.js
+++ backend/app.js
@@ -103,6 +103,22 @@ app.put('/api/stuff/:id', (req, res, next) => {
     );
   });
 
+  app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({_id: req.params.id}).then(
+      () => {
+        res.status(200).json({
+          message: 'Deleted!'
+        });
+      }
+    ).catch(
+      (error) => {
+        res.status(400).json({
+          error: error
+        });
+      }
+    );
+  });
+
 app.use('/api/stuff',(req, res, next) => {
     Thing.find().then((things)=>{
         res.status(200).json(things);
