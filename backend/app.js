e5747c873e44529ed5ebb6ccba9710804b32ace1
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-08-04 08:03:45 +0200

    update an existing thing

--- backend/app.js
+++ backend/app.js
@@ -79,6 +79,29 @@ app.get('/api/stuff/:id', (req, res, next) => {
     });
 });
 
+app.put('/api/stuff/:id', (req, res, next) => {
+    const thing = new Thing({
+      _id: req.params.id,
+      title: req.body.title,
+      description: req.body.description,
+      imageUrl: req.body.imageUrl,
+      price: req.body.price,
+      userId: req.body.userId
+    });
+    Thing.updateOne({_id: req.params.id}, thing).then(
+      () => {
+        res.status(201).json({
+          message: 'Thing updated successfully!'
+        });
+      }
+    ).catch(
+      (error) => {
+        res.status(400).json({
+          error: error
+        });
+      }
+    );
+  });
 
 app.use('/api/stuff',(req, res, next) => {
     Thing.find().then((things)=>{
