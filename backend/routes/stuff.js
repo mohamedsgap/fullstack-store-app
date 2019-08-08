a0c193bc00aff4096d2737f387719d9c0fee30a2
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-08-08 05:08:07 +0200

    refactor routes/stuff

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,88 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const Thing = require('../models/thing');
+
+const stuffCtrl = require('../controllers/stuff');
 
 
-router.post('/', (req, res, next) => {
-    const thing = new Thing({
-        title: req.body.title,
-        description: req.body.description,
-        imageUrl: req.body.imageUrl,
-        price: req.body.price,
-        userId: req.body.userId
-    });
-    thing.save().then(()=> {
-        res.status(201).json({
-            message: 'Post saved successfully!'
-        });
-    }).catch((error)=>{
-        res.status(400).json({
-            error: error
-        });
-    });
-});
+router.post('/', stuffCtrl.createThing);
 
-router.get('/:id', (req, res, next) => {
-    Thing.findOne({
-        _id: req.params.id
+router.get('/:id', stuffCtrl.getSingleThing);
 
-    }).then((thing)=>{
-        res.status(200).json(thing);
-    }).catch((error)=>{
-        res.status(400).json({
-            error: error
-        });
-    });
-});
+router.put('/:id', stuffCtrl.modifyThing);
 
-router.put('/:id', (req, res, next) => {
-    const thing = new Thing({
-      _id: req.params.id,
-      title: req.body.title,
-      description: req.body.description,
-      imageUrl: req.body.imageUrl,
-      price: req.body.price,
-      userId: req.body.userId
-    });
-    Thing.updateOne({_id: req.params.id}, thing).then(
-      () => {
-        res.status(201).json({
-          message: 'Thing updated successfully!'
-        });
-      }
-    ).catch(
-      (error) => {
-        res.status(400).json({
-          error: error
-        });
-      }
-    );
-  });
+router.delete('/:id', stuffCtrl.deleteThing);
 
-  router.delete('/:id', (req, res, next) => {
-    Thing.deleteOne({_id: req.params.id}).then(
-      () => {
-        res.status(200).json({
-          message: 'Deleted!'
-        });
-      }
-    ).catch(
-      (error) => {
-        res.status(400).json({
-          error: error
-        });
-      }
-    );
-  });
-
-router.get('/' + '',(req, res, next) => {
-    Thing.find().then((things)=>{
-        res.status(200).json(things);
-    }).catch((error)=>{
-        res.status(400).json({
-            error: error
-        });
-    });
-});
+router.get('/' + '', stuffCtrl.getAllthings);
 
 module.exports = router;
