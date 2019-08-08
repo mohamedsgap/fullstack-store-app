4fdd63579933ed08dd63e91682229ceafaa1c7a3
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-08-08 09:34:14 +0200

    create new user

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,23 @@
-exports.signup = (req, res, next) => {
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
 
+
+exports.signup = (req, res, next) => {
+    bcrypt.hash(req.body.password, 10).then((hass)=>{
+        const user = new User({
+            email: req.body.email,
+            password: hash
+        });
+        user.save().then(()=>{
+            res.status(201).json({
+                message: 'Successfully added new User'
+            });
+        }).catch((error)=>{
+            res.status(500).json({
+               error: error
+            });
+        });
+    });
 };
 
 exports.login = (req, res, next) => {
