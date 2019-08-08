c8517ceca7c9368efd4128e5a0a23839a54ac25d
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-08-08 04:43:35 +0200

    refactor express app

--- backend/app.js
+++ backend/app.js
@@ -4,31 +4,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const Thing = require('./models/thing');
+const stuffRouters = require('./routes/stuff');
 
 
 const app = express();
-/*
-app.use((req, res,next)=>{
-    console.log("The request received");
-    next();
-});
-
-app.use((req, res, next) =>{
-    res.status(201);
-    next();
-});
-
-app.use((req, res, next) => {
-    res.json({message: 'Your request was successfull! '});
-    next();
-});
-
-app.use((req, res, next)=>{
-    console.log("the rquest sent successfully");
-});
-*/
-
 
 mongoose.connect('mongodb+srv://mohamed:un0xlqfbTxUVH7In@cluster0-fytzm.mongodb.net/test?retryWrites=true&w=majority').then(() => {
     console.log("'Successfully connected to MongoDB Atlas!'");
@@ -47,87 +26,6 @@ app.use((req, res, next)=>{
 
 app.use(bodyParser.json());
 
-app.post('/api/stuff', (req, res, next) => {
-    const thing = new Thing({
-        title: req.body.title,
-        description: req.body.description,
-        imageUrl: req.body.imageUrl,
-        price: req.body.price,
-        userId: req.body.userId
-    });
-    thing.save().then(()=> {
-        res.status(201).json({
-            message: 'Post saved successfully!'
-        });
-    }).catch((error)=>{
-        res.status(400).json({
-            error: error
-        });
-    });
-});
-
-app.get('/api/stuff/:id', (req, res, next) => {
-    Thing.findOne({
-        _id: req.params.id
-
-    }).then((thing)=>{
-        res.status(200).json(thing);
-    }).catch((error)=>{
-        res.status(400).json({
-            error: error
-        });
-    });
-});
-
-app.put('/api/stuff/:id', (req, res, next) => {
-    const thing = new Thing({
-      _id: req.params.id,
-      title: req.body.title,
-      description: req.body.description,
-      imageUrl: req.body.imageUrl,
-      price: req.body.price,
-      userId: req.body.userId
-    });
-    Thing.updateOne({_id: req.params.id}, thing).then(
-      () => {
-        res.status(201).json({
-          message: 'Thing updated successfully!'
-        });
-      }
-    ).catch(
-      (error) => {
-        res.status(400).json({
-          error: error
-        });
-      }
-    );
-  });
-
-  app.delete('/api/stuff/:id', (req, res, next) => {
-    Thing.deleteOne({_id: req.params.id}).then(
-      () => {
-        res.status(200).json({
-          message: 'Deleted!'
-        });
-      }
-    ).catch(
-      (error) => {
-        res.status(400).json({
-          error: error
-        });
-      }
-    );
-  });
-
-app.use('/api/stuff',(req, res, next) => {
-    Thing.find().then((things)=>{
-        res.status(200).json(things);
-    }).catch((error)=>{
-        res.status(400).json({
-            error: error
-        });
-    });
-});
-
+app.use('/api/stuff', stuffRouters);
 
 module.exports = app;   
